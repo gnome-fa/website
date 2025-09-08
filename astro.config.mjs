c97c0f792e3df3284113da3384eb7200a675d2e0
hadi <hazard7546@gmail.com>
2025-09-08 21:45:00 +0330

    chore(astro): set base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import alpinejs from '@astrojs/alpinejs';
 // https://astro.build/config
 export default defineConfig({
   site:"https://persian.gnome.org",
+  // Temporary until moving to the GNOME domain
+  base:"/website",
   integrations: [alpinejs()]
 });
