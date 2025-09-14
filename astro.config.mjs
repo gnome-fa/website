6c23bc25097f682c4f7b95c39c1ad952b50ff4fe
sobhan <me@mirsobhan.ir>
2025-09-14 20:58:51 +0330

    add compresor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import alpinejs from '@astrojs/alpinejs';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
   site:"https://fa.gnome.org",
   outDir: 'public',
   publicDir: 'static',
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), playformCompress()]
 });
+
